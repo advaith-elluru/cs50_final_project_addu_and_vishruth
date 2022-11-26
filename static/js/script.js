edb471ba9778b77ad3ed6c2035e71ce338d871d0
advaith-elluru <76271827+advaith-elluru@users.noreply.github.com>
2022-11-26 01:15:53 +0000

    Sat, Nov 26, 2022, 1:15 AM +00:00

--- static/js/script.js
+++ static/js/script.js
@@ -14,20 +14,20 @@ function check_test()
     $("#table tr").each(function() {
         var client_answer = $(this).find("#client_answer").val();
         var answer = $(this).find(".answer").text();
-        var $this = $(this).eq(4);
+        var now = $(this).eq(4);
 
         if (client_answer == answer)
         {
             var color = $(this).eq(4).css("background-color");
             alert(color);
-            $(this).eq(4).css("background-color: green");
+            $(now).eq(4).css("background-color: green");
             correct++;
         }
         else
         {
             var color = $(this).eq(4).css("background-color");
             alert(color);
-            $(this).eq(4).css("background-color: red");
+            $(now).eq(4).css("background-color: red");
         }
     });
     alert(correct);
