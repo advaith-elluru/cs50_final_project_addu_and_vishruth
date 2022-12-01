cbea344433a66194ee3cc19e769ad8f963b38537
advaith-elluru <76271827+advaith-elluru@users.noreply.github.com>
2022-12-01 02:39:43 +0000

    Thu, Dec 1, 2022, 2:39 AM +00:00

--- static/js/script.js
+++ static/js/script.js
@@ -64,8 +64,8 @@ function check_test(decision, pronums, time)
     $("#percent_correct").text(parseInt(correct) / parseInt(pronums)) + "%";
     $("#time_taken").text((parseInt(time) / 1000) + " seconds")
 
-    $("#table").css("display", "inline-block");
-    $("#table_1").css("display", "inline-block");
+    // $("#table").css("display", "inline-block");
+    $("#table_1").css("display", "inline");
     $("#table_1").show();
 }
 
