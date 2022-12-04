700d7266fafd536fefe8bb7a4ff652c68d36f775
advaith-elluru <76271827+advaith-elluru@users.noreply.github.com>
2022-12-04 17:12:09 +0000

    Sun, Dec 4, 2022, 5:12 PM +00:00

--- static/js/script.js
+++ static/js/script.js
@@ -69,7 +69,7 @@ function check_test(decision, pronums, time, avgTime)
     $("#table_1").css("display", "inline-block");
     $("#table_1").show();
 
-    x = {}
+    x = {"number_correct": parseInt(correct), "number_of_questions": parseInt(pronums), "percent_correct": (parseInt(correct) / parseInt(pronums)).toFixed(6) * 100, "time": parseFloat(time), "avg_time": parseFloat(avgTime).toFixed(4)};
     $.post("/results", json.stringify(x));
 }
 
