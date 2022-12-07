955442f1d51709f05c8f86fa6f0e8cb3c6ddeba0
advaith-elluru <76271827+advaith-elluru@users.noreply.github.com>
2022-12-07 21:48:34 +0000

    Wed, Dec 7, 2022, 9:48 PM +00:00

--- app.py
+++ app.py
@@ -270,7 +270,7 @@ def past_results():
         if len(rows) < 1:
             return apology("You didn't take any tests", 404)
         hi = len(rows)
-        return render_template("choose.html", rows = len(rows))
+        return render_template("choose.html", rows = len(rows), hi = hi)
 
 
 @app.route("/credit", methods=["GET", "POST"])
