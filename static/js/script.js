d6c46a4b40c29b1313b3249949913d9b4358301a
vishhub <76272056+vishruth-pesala@users.noreply.github.com>
2023-02-21 01:52:37 +0000

    Tue, Feb 21, 2023, 1:52 AM +00:00

--- static/js/script.js
+++ static/js/script.js
@@ -130,6 +130,7 @@ function chk_ws_test(number, seconds, avgTime){
     document.querySelector('#time_taken').innerHTML = seconds;
     document.querySelector('#percent_correct').innerHTML = percentCorrect;
     document.querySelector('#number_correct').innerHTML = correct_ans;
+    document.querySelector('')
     var time = seconds;
     var avg_Time = avgTime;
     // Var
