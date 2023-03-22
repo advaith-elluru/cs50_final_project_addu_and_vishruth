c0149e57b289fedf25ab5c49884efd93642e83a8
Advaith Elluru <76271827+advaith-elluru@users.noreply.github.com>
2023-03-22 18:06:51 +0000

    Wed, Mar 22, 2023, 2:06 PM -04:00

--- app.py
+++ app.py
@@ -29,18 +29,6 @@ def before_request():
     session.permanent = True
     app.permanent_session_lifetime = datetime.timedelta(minutes=120)
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
-
-def apology(message, code):
-    return render_template("apology.html", message = message, code = code)
-
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
